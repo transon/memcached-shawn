de326831beec07d45fae4aea89d464e3e6baaca1
songheyang <songheyang@cf29242b-7f17-630f-c418-a0f6ef87f6ad>
2010-09-20 13:09:42 +0000

    add mem$d source code

---
+++ trunk/Debug/assoc.d
@@ -0,0 +1,25 @@
+assoc.d assoc.o: ../assoc.c ../memcached.h ../protocol_binary.h \
+ ../cache.h ../sasl_defs.h ../stats.h ../slabs.h ../assoc.h ../items.h \
+ ../trace.h ../hash.h ../util.h
+
+../memcached.h:
+
+../protocol_binary.h:
+
+../cache.h:
+
+../sasl_defs.h:
+
+../stats.h:
+
+../slabs.h:
+
+../assoc.h:
+
+../items.h:
+
+../trace.h:
+
+../hash.h:
+
+../util.h:
